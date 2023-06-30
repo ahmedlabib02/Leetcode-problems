f3cb6225128e1c5bca3a49d1c711c14edc0bb871
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-06-30 19:46:04 +0300

    Time: 1 ms (90.67%), Space: 44.3 MB (45.51%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+      boolean changed=false;
+        for(int i=0;i<nums.length-1;i++)
+            if(nums[i]>nums[i+1]){
+                if(changed)
+                    return false;
+               if(i==0){ 
+                       changed = true;
+               }
+                else{
+                    if(nums[i-1]>nums[i+1] )
+                        nums[i+1]=nums[i];
+                
+                    changed=true;
+                        
+                }
+                    
+                
+                   
+                       
+            }
+                 
+        return true;
+    }
+}
