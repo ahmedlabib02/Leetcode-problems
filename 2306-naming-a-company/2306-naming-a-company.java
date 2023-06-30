badc4079f1f6c65c44e67cba0ac9f533fca3d067
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-06-30 18:09:19 +0300

    Time: 184 ms (52.26%), Space: 55.6 MB (38.71%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.java
+++ 2306-naming-a-company/2306-naming-a-company.java
@@ -13,11 +13,12 @@ class Solution {
             else 
                 map.get(idea.charAt(0)).add(zeby);
         } 
+        HashSet<Character> done= new HashSet<Character>();
         long res=0;
         for(Character c1 : map.keySet()){
             HashSet<String> set1 = map.get(c1);
             for(Character c2 : map.keySet()){
-                if(c1==c2 ) continue;
+                if(c1==c2 || done.contains(c2)) continue;
                 HashSet<String> set2 = map.get(c2);
                 int counter =0;
                 for(String s :  set1)
@@ -25,8 +26,9 @@ class Solution {
                         counter++;
                 int x = set1.size() - counter;    
                 int y = set2.size() - counter;
-                res+= (x*y);
+                res+= (x*y)*2;
             }
+            done.add(c1);
             
         }
          
