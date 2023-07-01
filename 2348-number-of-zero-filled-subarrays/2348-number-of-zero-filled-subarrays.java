3e2e1224ed43e062709f0535dd3f4c49d41dff41
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-01 16:42:20 +0300

    Time: 3 ms (100.00%), Space: 56.4 MB (92.69%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -3,14 +3,10 @@ class Solution {
         long sequence = 0;
         long count =0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                sequence++;
-                count+=sequence;
-            }
-            else {
-                sequence=0;
-            }
-                
+            if(nums[i]==0)
+                count+=++sequence;
+            else 
+                sequence=0;        
         }
         return count;
     }
