3d00f19d2e45e8b6a60bc0a2c2800c5863ecce93
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-04 02:40:56 +0300

    Time: 18 ms (79.98%), Space: 54.4 MB (11.75%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -5,23 +5,18 @@ class Solution {
         int i =0;
         int j= people.length-1;
         while(i<=j){
-            if(people[i]==-1)
-                i++;
             if(people[i]+people[j]<=limit){
                 boat++;
-                people[j]=-1;
-                people[i]=-1;
                 i++;
                 j--;
             }
             else if(people[i]+people[j]>limit){
                 j--;
+                boat++;
             }
                 
         }
-        for(int x=0;x<people.length;x++)
-            if(people[x]!=-1)
-                boat++;
+        
         
         return boat;
         
