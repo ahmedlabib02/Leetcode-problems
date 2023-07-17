dd4ad4e5d5e4a1f19029b3112f808f8daf7ad774
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-17 14:28:17 +0300

    Time: 384 ms (5.01%), Space: 68.9 MB (77.20%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,50 @@
+class Solution {
+    HashMap<Integer,ArrayList<Integer>> map;
+    HashMap<Integer,ArrayList<Integer>> map2;
+    boolean[] seen;
+    int ans;
+    public int minReorder(int n, int[][] connections) {
+        map=new HashMap<>();
+        map2= new HashMap<>();
+        seen = new boolean[n];
+        seen[0]= true;
+        
+        for(int i=0;i<connections.length;i++)
+        {
+            if( map2.get(connections[i][0])==null )
+                map2.put(connections[i][0],new ArrayList<>());
+            if(map.get(connections[i][1])==null)
+                map.put(connections[i][1],new ArrayList<>());
+            map.get(connections[i][1]).add(connections[i][0]);
+            map2.get(connections[i][0]).add(connections[i][1]);
+            
+        }
+        
+        dfs(0);
+        
+        System.out.println(map);
+        System.out.println(map2);
+        return ans;
+    }
+    
+    public void dfs(int i){
+        if(map.get(i)!=null)
+        for(int num: map.get(i))
+        {
+            seen[num]=true;
+            dfs(num);
+        }
+                
+        
+        if(map2.get(i)!=null)
+        for(int num : map2.get(i))
+           if(!seen[num])
+            {
+            seen[num]=true;   
+            ans++;
+            dfs(num);
+            }
+            
+    }
+    
+}
