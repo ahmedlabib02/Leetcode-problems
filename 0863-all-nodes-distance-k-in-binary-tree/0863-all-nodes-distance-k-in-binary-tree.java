3813af16d81c014ee2ae143d8a0d73b2221a38c9
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-18 18:37:30 +0300

    Time: 19 ms (6.01%), Space: 43.3 MB (11.84%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class state {
+    int step;
+    TreeNode node;
+    public state(TreeNode node,int step){
+        this.step = step;
+        this.node= node;
+    }
+}
+class Solution {
+    HashMap<TreeNode,TreeNode> map ;
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        map = new HashMap<>();
+        dfs(root);
+        Queue<state> q = new LinkedList<>();
+        System.out.print(map);
+        q.add(new state(target,0));
+        ArrayList<Integer> ans =new ArrayList<Integer>();
+        HashSet<TreeNode> seen = new HashSet<>();
+        seen.add(target);
+        while(!q.isEmpty())
+        {
+            state s = q.remove();
+            if(s.step==k)
+                ans.add(s.node.val);
+            TreeNode parent = map.get(s.node);
+            TreeNode left = s.node.left;
+            TreeNode right = s.node.right;
+            if(parent!=null && !seen.contains(parent)){
+                seen.add(parent);
+                q.add(new state(parent,s.step+1));
+            }   
+            if(left!=null&& !seen.contains(left)){
+                seen.add(left);
+                q.add(new state(left,s.step+1));
+            }
+            if(right!=null && !seen.contains(right)){
+                seen.add(right);
+                q.add(new state(right,s.step+1));
+            }
+                
+            
+        }
+        return ans;
+    }
+    public void dfs(TreeNode root){
+        if(root.left!=null)
+        {
+            map.put(root.left,root);
+            dfs(root.left);
+        }
+        
+        if(root.right!=null)
+        {
+            map.put(root.right,root);    
+            dfs(root.right);
+        }
+       
+    }
+    
+}
