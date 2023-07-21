030b10b3f32a6823aa0ed8c2e8a5941472ff72f2
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-21 14:15:08 +0300

    Time: 1 ms (100.00%), Space: 43.7 MB (74.15%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -8,20 +8,22 @@ class Solution {
         return ans;
     }
     public void backtrack(List<Integer> curr, List<List<Integer>>ans, int[] nums , int index){
-        if(!seen.contains(curr))
-        {
-            seen.add(curr);
-            ans.add(new ArrayList<>(curr));
-        }
         
+            
+        ans.add(new ArrayList<>(curr));
         if(index==nums.length-1)
             return;
         
         for(int i=index+1;i<nums.length;i++)
         {
+            if(i==index+1 ||i!=index+1 && nums[i]!=nums[i-1])
+            {
+              
             curr.add(nums[i]);
             backtrack(curr,ans,nums,i);
             curr.remove(curr.size()-1);
+            }
+            
         }
     }
     
