5016f695141644ee208b527da77bfdb6c0832f3d
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-23 01:26:47 +0300

    Time: 39 ms (5.06%), Space: 44.9 MB (5.58%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,41 @@
+class Solution {
+    int n;
+    public List<List<String>> solveNQueens(int n) {
+        this.n =n;
+        List<List<String>> ans = new ArrayList<>();
+        HashSet<Integer> ver = new HashSet<Integer>();
+        HashSet<Integer> dia = new HashSet<Integer>();
+        HashSet<Integer> anti = new HashSet<Integer>();
+        backtrack(new ArrayList<>(), ans,0, ver, dia, anti);
+        return ans;
+    }
+    public void backtrack(List<String> curr, List<List<String>> ans, int hor,HashSet<Integer> ver , HashSet<Integer> dia,       HashSet<Integer> anti){
+        
+        if(hor==n)
+        {
+          ans.add(new ArrayList<>(curr));
+          return;  
+        }
+        StringBuilder s= new StringBuilder();
+        for(int i=0;i<n;i++)
+            s.append('.');
+        System.out.println(curr);
+        for(int i=0; i<n; i++)
+        {
+            if(ver.contains(i) || dia.contains(hor-i) || anti.contains(hor+i))
+                continue;
+            
+            s.setCharAt(i,'Q');
+            ver.add(i);
+            dia.add(hor-i);
+            anti.add(hor+i);
+            curr.add(s.toString());
+            backtrack(curr, ans, hor+1,ver,dia,anti);
+            s.setCharAt(i,'.');
+            ver.remove(i);
+            dia.remove(hor-i);
+            anti.remove(hor+i);
+            curr.remove(curr.size()-1);  
+        }
+    } 
+}
