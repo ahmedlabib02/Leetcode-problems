e71032b5615333d90795f3ff984493ae75e5f2e6
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-23 16:00:27 +0300

    Time: 51 ms (5.33%), Space: 44 MB (71.57%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,42 @@
+class Solution {
+    
+    boolean[] seen;
+    HashMap<Integer, ArrayList<Integer>> map;
+    
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] inDegree = new int[numCourses];
+        map = new HashMap<>();
+        for(int[] edge : prerequisites)
+        {
+            int x = edge[0];
+            int y= edge[1];
+            if(map.get(x)==null )
+                map.put(x,new ArrayList<>());
+            map.get(x).add(y); 
+            inDegree[y]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<inDegree.length;i++)
+            if(inDegree[i]==0)
+                q.add(i);
+        
+         for(int i=0;i<numCourses;i++)   
+         {
+             System.out.println(q);
+             if(q.isEmpty())
+                 return false;
+             int num = q.remove();
+             for(int x : map.getOrDefault(num,new ArrayList<>()))
+             {
+                 inDegree[x]--;
+                 if(inDegree[x]==0)
+                     q.add(x);
+             }
+             
+         }
+        return true;
+    }
+    
+    
+    
+}
