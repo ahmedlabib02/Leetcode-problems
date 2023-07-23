ca0e4f126c4c3219ca81c35f1dd63a32e05205b7
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-23 19:01:05 +0300

    Time: 0 ms (100.00%), Space: 39.3 MB (55.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,34 @@
+// class Solution {
+//          HashMap<Integer,Integer> memo;
+//     public int climbStairs(int n) {
+//         memo= new HashMap<>();
+//         return dp(n);
+//     }
+//     public int dp(int n){
+//          if(n==1)
+//             return 1;
+//         if(n==2)
+//             return 2;
+//         if(memo.get(n)!=null)
+//             return memo.get(n);
+//         memo.put(n, dp(n-1)+ dp(n-2));
+//         return memo.get(n); 
+//     }
+// }
 class Solution {
-         HashMap<Integer,Integer> memo;
+         
     public int climbStairs(int n) {
-        memo= new HashMap<>();
-        return dp(n);
+        int[] memo = new int[n+1];
+        memo[1] = 1;
+        if(n==1)
+            return memo[1];
+        memo[2]= 2;
+        for(int i=3; i<=n;i++)
+        {
+            memo[i] = memo[i-2]+memo[i-1];
+        }
+        return memo[n];
     }
-    public int dp(int n){
-         if(n==1)
-            return 1;
-        if(n==2)
-            return 2;
-        if(memo.get(n)!=null)
-            return memo.get(n);
-        memo.put(n, dp(n-1)+ dp(n-2));
-        return memo.get(n); 
-    }
+   
 }
+
