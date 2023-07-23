b2043ca07d055526ef364077565e27b5ad9e0396
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-23 16:09:43 +0300

    Time: 3 ms (94.63%), Space: 44.4 MB (32.48%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,41 +1,42 @@
 class Solution {
-    
-    boolean[] seen;
-    HashMap<Integer, ArrayList<Integer>> map;
-    
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] inDegree = new int[numCourses];
-        map = new HashMap<>();
-        for(int[] edge : prerequisites)
-        {
-            int x = edge[0];
-            int y= edge[1];
-            if(map.get(x)==null )
-                map.put(x,new ArrayList<>());
-            map.get(x).add(y); 
-            inDegree[y]++;
+    public boolean dfs(int node, List<List<Integer>> adj, boolean[] visit, boolean[] inStack) {
+        // If the node is already in the stack, we have a cycle.
+        if (inStack[node]) {
+            return true;
         }
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<inDegree.length;i++)
-            if(inDegree[i]==0)
-                q.add(i);
-        
-         for(int i=0;i<numCourses;i++)   
-         {
-             if(q.isEmpty())
-                 return false;
-             int num = q.remove();
-             for(int x : map.getOrDefault(num,new ArrayList<>()))
-             {
-                 inDegree[x]--;
-                 if(inDegree[x]==0)
-                     q.add(x);
-             }
-             
-         }
+        if (visit[node]) {
+            return false;
+        }
+        // Mark the current node as visited and part of current recursion stack.
+        visit[node] = true;
+        inStack[node] = true;
+        for (int neighbor : adj.get(node)) {
+            if (dfs(neighbor, adj, visit, inStack)) {
+                return true;
+            }
+        }
+        // Remove the node from the stack.
+        inStack[node] = false;
+        return false;
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>(numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] prerequisite : prerequisites) {
+            adj.get(prerequisite[1]).add(prerequisite[0]);
+        }
+
+        boolean[] visit = new boolean[numCourses];
+        boolean[] inStack = new boolean[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            if (dfs(i, adj, visit, inStack)) {
+                return false;
+            }
+        }
         return true;
     }
-    
-    
-    
 }
