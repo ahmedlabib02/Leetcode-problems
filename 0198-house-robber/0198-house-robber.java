8a65a00380c855340dbd1bf8ec60065380a5794b
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-23 19:25:39 +0300

    Time: 0 ms (100.00%), Space: 40 MB (65.71%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,28 @@
+class Solution {
+    int[] memo;
+    public int rob(int[] nums) {
+        memo = new int[nums.length];
+        Arrays.fill(memo,-1);
+        if(nums.length==1)
+            return nums[0];
+        
+        return Math.max(dp(nums.length-1,nums), dp(nums.length-2,nums));
+    }
+    public int dp(int i, int[] nums)
+    {
+        if(i==0){
+            memo[0]= nums[0];
+            return nums[0];
+        }
+        if(i==1)
+        {
+            memo[1] = Math.max(nums[0],nums[1]);
+            return memo[1];
+        }
+        if(memo[i]!=-1)
+            return memo[i];
+        memo[i]= Math.max(dp(i-2,nums)+ nums[i], dp(i-1,nums));
+        return memo[i];
+        
+    }
+}
