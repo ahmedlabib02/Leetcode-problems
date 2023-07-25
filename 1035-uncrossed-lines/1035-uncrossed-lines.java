7f80cb07f1ad14a307d6013bac9348c06023b3b2
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-25 12:00:04 +0300

    Time: 10 ms (16.08%), Space: 42.9 MB (80.93%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -1,31 +1,32 @@
 class Solution {
-    private int solve(int i, int j, int[] nums1, int[] nums2, int[][] memo) {
-        if (i <= 0 || j <= 0) {
-            return 0;
-        }
-
-        if (memo[i][j] != -1) {
-            return memo[i][j];
-        }
-
-        if (nums1[i - 1] == nums2[j - 1]) {
-            memo[i][j] = 1 + solve(i - 1, j - 1, nums1, nums2, memo);
-        } else {
-            memo[i][j] =
-                Math.max(solve(i, j - 1, nums1, nums2, memo), solve(i - 1, j, nums1, nums2, memo));
-        }
-        return memo[i][j];
-    }
-
+    int[][] memo;
+    int[] nums1;
+    int[] nums2;
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-
-        int[][] memo = new int[n1 + 1][n2 + 1];
-        for (int[] row : memo) {
-            Arrays.fill(row, -1);
-        }
-
-        return solve(n1, n2, nums1, nums2, memo);
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        memo = new int[nums1.length][nums2.length];
+        for(int[] arr : memo)
+            Arrays.fill(arr,-1);
+        return dp(0,0);
     }
+    public int dp(int i, int j){
+        if(i>nums1.length-1|| j>nums2.length-1)
+            return 0;
+        if(memo[i][j]!=-1)
+            return memo[i][j];
+        if(nums1[i]==nums2[j])
+        {
+            int temp = Math.max(dp(i,j+1), dp(i+1,j ) );
+            memo[i][j] = Math.max(1+dp(i+1,j+1), temp);
+        }
+        else 
+        {
+            memo[i][j] = Math.max(dp(i+1,j), dp(i,j+1));
+        }
+        
+        return memo[i][j];
+            
+    }
+    
 }
