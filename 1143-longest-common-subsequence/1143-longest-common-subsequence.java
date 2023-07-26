9826bab97b1845c27c58ac0ed862b7753e0ca9b2
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-26 15:48:55 +0300

    Time: 42 ms (25.92%), Space: 48.3 MB (63.39%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,29 +1,40 @@
 class Solution {
+    int m;
+    int n;
     int[][] memo;
-    String s1;
-    String s2;
+    String text1;
+    String text2;
+    
     public int longestCommonSubsequence(String text1, String text2) {
-        memo = new int[text1.length()][text2.length()];
-        for(int[] arr: memo)
-            Arrays.fill(arr,-1);
-        this.s1=text1;
-        this.s2=text2;
-        return dp(0,0);
-    }
-    public int dp(int i,int j){
-        if(i==s1.length()|| j==s2.length())
-            return 0;
-        if(memo[i][j]!=-1)
-            return memo[i][j];
-        int max = 0;
-        if(s1.charAt(i)==s2.charAt(j))
-            max = Math.max(1+dp(i+1,j+1), max);
-        else
-        {
-            max = Math.max(dp(i,j+1) ,max);
-            max = Math.max(dp(i+1,j) ,max);
+        m = text1.length();
+        n = text2.length();
+        this.text1 = text1;
+        this.text2 = text2;
+        memo = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(memo[i], -1);
         }
-        memo[i][j]= max;
-        return memo[i][j];
+        
+        return dp(0, 0);
+    }
+    
+    public int dp(int i, int j) {
+        if (i == m || j == n) {
+            return 0;
+        }
+        
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+        
+        int ans = 0;
+        if (text1.charAt(i) == text2.charAt(j)) {
+            ans = 1 + dp(i + 1, j + 1);
+        } else {
+            ans = Math.max(dp(i + 1, j), dp(i, j + 1));
+        }
+        
+        memo[i][j] = ans;
+        return ans;
     }
 }
