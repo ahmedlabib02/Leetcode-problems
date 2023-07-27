301da53b2ca38be807af2ecce39eea0d77c54240
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-27 14:07:23 +0300

    Time: 10 ms (19.95%), Space: 54.6 MB (22.60%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,89 +1,40 @@
 class Solution {
-    int memo[][];
+    int[][] memo;
     char[][] arr;
+
     public int maximalSquare(char[][] matrix) {
-        memo = new int[matrix.length][matrix[0].length];
-        for(int[] x: memo)
-            Arrays.fill(x,-1);
-        int max=0;
-        arr= matrix;
-        for(int i=0;i<arr.length;i++)
-            for(int j=0;j<arr[i].length;j++)
-            {
-                if(memo[i][j]==-1 && arr[i][j]!='0')
-                    max = Math.max(max, dp(i,j) );
-                else
-                    max= Math.max(max,memo[i][j]);
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        memo = new int[rows][cols];
+        arr = matrix;
+        int max = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (memo[i][j] == 0 && arr[i][j] != '0') {
+                    max = Math.max(max, dp(i, j));
+                } else {
+                    max = Math.max(max, memo[i][j]);
+                }
             }
-        // for(int i=0;i<arr.length;i++)
-        // {
-        //     for(int j=0;j<arr[i].length;j++)
-        //         System.out.print(memo[i][j]+"   ");
-        //     System.out.println();
-        // }
-            
+        }
+
         return max;
     }
-    public int dp(int i, int j ){
-        // System.out.println(i+" "+j);
-        if(memo[i][j]!=-1)
-            return memo[i][j];
-        int down= 1;
-        if(i!=arr.length-1 )
-        {
-            if(arr[i+1][j]=='0')
-            {
-                memo[i][j]=1;
-                return memo[i][j];
-            }   
-            else
-                down = dp(i+1,j);
-        }
-        else 
-        {
-            memo[i][j]=1;
-            return 1;
-        }
-        int right = 0;
-        if(j!=arr[i].length-1)
-        {
-            if(arr[i][j+1]=='0')
-            {
-                memo[i][j]=1;
-                return memo[i][j]; 
-            }
-                
-            else
-                right= dp(i,j+1);   
-        }
-        else
-        {
-            memo[i][j]=1;
-            return 1;
-        }
-        int dia = 0;
-        if(j!=arr[i].length-1 && i!=arr.length-1)
-        {
-            if(arr[i+1][j+1]=='0' )
-            {
-                memo[i][j]=1;
-                return memo[i][j]; 
-            }
-                
-            else
-                dia = dp(i+1,j+1); 
-        }
-        else 
-        {
-            memo[i][j]=1;
-            return 1;
-        }
-        
-        int temp = Math.min(down,Math.min(dia,right));
-        temp = (int)Math.sqrt(temp);
-        temp++;
-        memo[i][j]= temp*temp;
-        return memo[i][j];
 
+    public int dp(int i, int j) {
+        if (memo[i][j] != 0)
+            return memo[i][j];
+
+        int down = (i != arr.length - 1 && arr[i + 1][j] != '0') ? dp(i + 1, j) : 0;
+        int right = (j != arr[i].length - 1 && arr[i][j + 1] != '0') ? dp(i, j + 1) : 0;
+        int dia = (i != arr.length - 1 && j != arr[i].length - 1 && arr[i + 1][j + 1] != '0') ? dp(i + 1, j + 1) : 0;
+
+        int temp = Math.min(down, Math.min(dia, right));
+        temp = (int) Math.sqrt(temp);
+        temp++;
+        memo[i][j] = temp * temp;
+
+        return memo[i][j];
     }
 }
