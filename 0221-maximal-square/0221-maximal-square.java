a6d93faabea9ff37d66678e2d0a00dba31c066b5
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-27 14:06:23 +0300

    Time: 13 ms (15.40%), Space: 54.5 MB (22.60%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -15,12 +15,12 @@ class Solution {
                 else
                     max= Math.max(max,memo[i][j]);
             }
-        for(int i=0;i<arr.length;i++)
-        {
-            for(int j=0;j<arr[i].length;j++)
-                System.out.print(memo[i][j]+"   ");
-            System.out.println();
-        }
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //     for(int j=0;j<arr[i].length;j++)
+        //         System.out.print(memo[i][j]+"   ");
+        //     System.out.println();
+        // }
             
         return max;
     }
