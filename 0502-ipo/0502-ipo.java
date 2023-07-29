46a9d11d482c6a8b5275b76b625bf93df321c037
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-07-30 00:51:09 +0300

    Time: 91 ms (94.71%), Space: 63.4 MB (15.43%) - LeetHub

---
+++ 0502-ipo/0502-ipo.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int ans = w;
+        Comparator<int[]> pairComparator = new Comparator<>() {
+    @Override
+        public int compare(int[] a, int[] b) {
+        if (a[1] != b[1]) 
+            return a[1] - b[1]; // sort on capital in ascending order
+        else 
+            return b[0] - a[0]; // sort on profit in descending order
+        }
+    };
+
+        Comparator<int[]> comp = new Comparator<>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return b[0] - a[0];    //sort on profit
+            }
+        };
+        PriorityQueue<int[]> pq = new PriorityQueue<>(pairComparator);
+        PriorityQueue<int[]> max = new PriorityQueue<>(comp);
+        for (int i = 0; i < profits.length; i++) {
+            int[] pair = new int[2];
+            pair[0] = profits[i];
+            pair[1] = capital[i];
+            pq.add(pair);
+        }
+        
+        while(!pq.isEmpty())
+        {
+            int[] temp = pq.peek();
+            if(temp[1]>w)
+            {
+                if(max.isEmpty())
+                    return ans;
+                int num = max.poll()[0];
+                ans+= num;
+                w+= num;
+                k--;
+            }
+            else
+            {
+                max.add(pq.poll());
+            }
+            if(k==0)
+                return ans;
+        }
+        while(k>0&& !max.isEmpty())
+        {
+            ans+=max.poll()[0];
+            k--;
+        }
+            
+        
+       return ans; 
+    }
+    
+    public static void printPriorityQueue(PriorityQueue<int[]> pq) {
+        PriorityQueue<int[]> copy = new PriorityQueue<>(pq);
+        System.out.println("Contents of the PriorityQueue:");
+        while (!copy.isEmpty()) {
+            int[] pair = copy.poll();
+            System.out.println("[" + pair[0] + ", " + pair[1] + "]");
+        }
+    }
+}
