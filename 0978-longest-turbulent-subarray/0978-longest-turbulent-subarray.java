96fc03dbe9617292ed2aac93bebbc288a5af1724
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-08-03 17:23:37 +0300

    Time: 512 ms (5.23%), Space: 47.4 MB (29.55%) - LeetHub

---
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+        boolean asc=true;
+        if(arr.length==1)
+            return 1;
+        if(arr[0]>arr[1])
+            asc=false;
+        int i = 0;
+        int ans = 1; // Initialize ans to 1, as a single element is considered a valid sequence
+        int count = 1;
+
+        while (i < arr.length - 1) {
+            System.out.println(count+" "+asc+" "+arr[i]);
+            if(arr[i] < arr[i + 1]) 
+            {
+                if (asc) 
+                {
+                    count++;
+                    asc=false;
+                }   
+                else 
+                {
+                    ans= Math.max(count,ans);
+                    count = 2;
+                }
+                    
+            } 
+            else if(arr[i] > arr[i + 1]) 
+            {
+                if (!asc) 
+                {
+                    count++;
+                    asc=true;
+                    
+                } 
+                else
+                {
+                    ans=Math.max(count,ans);
+                    count = 2; 
+                }
+            } 
+            else 
+            {
+                ans=Math.max(count,ans);
+                count=1;
+                if(i<arr.length-2&&arr[i+1]>arr[i+2])
+                    asc=false;
+                else
+                    asc=true;
+            }
+            i++;
+            
+        }
+        ans =Math.max(count,ans);
+        return ans;
+    }
+}
