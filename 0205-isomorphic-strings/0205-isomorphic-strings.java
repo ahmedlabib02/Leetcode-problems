e9a2fd2a4d1549467d9da13006dcf07e32236f71
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-08-13 01:18:27 +0300

    Time: 5 ms (96.09%), Space: 41.4 MB (81.59%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,20 +1,29 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length()!=t.length())
-            return false;
-        HashMap<Character, Character> map = new HashMap<>();
-        HashMap<Character , Character> map2= new HashMap<>();
-        for(int i=0;i<s.length();i++)
-        {
-            if(map.get(s.charAt(i)) == null)
-                map.put(s.charAt(i),t.charAt(i));
-            else if(map.get(s.charAt(i)) != t.charAt(i))
+        
+        int[] mappingDictStoT = new int[256];
+        Arrays.fill(mappingDictStoT, -1);
+        
+        int[] mappingDictTtoS = new int[256];
+        Arrays.fill(mappingDictTtoS, -1);
+        
+        for (int i = 0; i < s.length(); ++i) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            
+            // Case 1: No mapping exists in either of the dictionaries
+            if (mappingDictStoT[c1] == -1 && mappingDictTtoS[c2] == -1) {
+                mappingDictStoT[c1] = c2;
+                mappingDictTtoS[c2] = c1;
+            }
+            
+            // Case 2: Ether mapping doesn't exist in one of the dictionaries or Mapping exists and
+            // it doesn't match in either of the dictionaries or both 
+            else if (!(mappingDictStoT[c1] == c2 && mappingDictTtoS[c2] == c1)) {
                 return false;
-            if(map2.get(t.charAt(i))==null)
-                map2.put(t.charAt(i), s.charAt(i));
-            else if(map2.get(t.charAt(i)) != s.charAt(i))
-                return false;
+            }
         }
+        
         return true;
     }
 }
