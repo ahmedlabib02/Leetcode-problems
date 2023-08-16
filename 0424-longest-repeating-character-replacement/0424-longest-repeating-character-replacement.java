00b28c0b0b8b67a890b4e6b9289b1aca88c349b2
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-08-16 03:05:22 +0300

    Time: 11 ms (44.99%), Space: 41.2 MB (95.42%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,25 +1,40 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int length=0;
-        HashMap<Character,Integer> map = new HashMap<>();
-        int i =0;
-        int max= 0;
-        for(int j=0;j<s.length();j++)
-        {
-            
-            map.put(s.charAt(j) , map.getOrDefault(s.charAt(j),0)+1 );
-            max = Math.max(max,map.get(s.charAt(j)));
-            int count = j-i+1-max;
-            // System.out.println(map+ " "+count);
-            if(count>k)
-            {
-                
-                map.put(s.charAt(i), map.get(s.charAt(i))-1 );
-                i++;
+        int start = 0;
+        int[] frequencyMap = new int[26];
+        int maxFrequency = 0;
+        int longestSubstringLength = 0;
+
+        for (int end = 0; end < s.length(); end += 1) {
+            // if 'A' is 0, then what is the relative order
+            // or offset of the current character entering the window
+            // 0 is 'A', 1 is 'B' and so on
+            int currentChar = s.charAt(end) - 'A';
+
+            frequencyMap[currentChar] += 1;
+
+            // the maximum frequency we have seen in any window yet
+            maxFrequency = Math.max(maxFrequency, frequencyMap[currentChar]);
+
+            // move the start pointer towards right if the current
+            // window is invalid
+            Boolean isValid = (end + 1 - start - maxFrequency <= k);
+            if (!isValid) {
+                // offset of the character moving out of the window
+                int outgoingChar = s.charAt(start) - 'A';
+
+                // decrease its frequency
+                frequencyMap[outgoingChar] -= 1;
+
+                // move the start pointer forward
+                start += 1;
             }
-            
-            length = Math.max(j-i+1,length);
+
+            // the window is valid at this point, note down the length
+            // size of the window never decreases
+            longestSubstringLength = end + 1 - start;
         }
-        return length;
+
+        return longestSubstringLength;
     }
 }
