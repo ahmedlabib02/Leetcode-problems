d5fe5612b12c72f409249a51ec17ca7f17383501
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-08-19 16:08:20 +0300

    Time: 4 ms (91.66%), Space: 45 MB (74.27%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,24 +1,40 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        if (arr.length <= k)
-            return Arrays.asList(Arrays.stream(arr).boxed().toArray(Integer[]::new));
-        int j=k;
-        int i=0;
-        while(j<arr.length){
-            if(Math.abs(arr[i]-x)>Math.abs(arr[j]-x)){
+        int index = getNum(arr,x);
+        ArrayList<Integer> list = new ArrayList<>();
+        int i=index;
+        int j = index;
+        k--;
+        while(k!=0)
+        {
+            if(i==0 )
                 j++;
-                i++;
-            }
-            else if(Math.abs(arr[i]-x)==Math.abs(arr[j]-x)&& arr[j]<=arr[i]){
-                   j++;
-                   i++;  
-            }
-            else break;
-    
+            else if(j==arr.length-1)
+                i--;
+            else if(x-arr[i-1]>arr[j+1]-x)
+                j++;
+            else i--;
+            k--;    
         }
-        List<Integer> list = new ArrayList<>();
-        for(int start=i;start<j;start++)
-            list.add(arr[start]);
+        for(int y=i;y<=j;y++)
+            list.add(arr[y]);
         return list;
+    }
+    
+    public int getNum(int[] nums,int target)
+    {
+        int i =0;
+        int j= nums.length-1;
+        while(i+1<j)
+        {
+            int mid = i+(j-i)/2;
+            if(nums[mid]> target)
+                j=mid;
+            else 
+                i=mid;
+        }
+        if(target-nums[i]>nums[j]-target)
+            return j;
+        return i;
     }
 }
