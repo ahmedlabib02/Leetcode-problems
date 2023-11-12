689b9a532d48fc1402360acee550a034139bc0b0
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2023-11-12 17:09:31 +0200

    Time: 3 ms (43.93%), Space: 44.5 MB (21.06%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    Boolean[] memo;
+    int[] arr;
+
+    public boolean canJump(int[] nums) {
+        int max=0;
+        int n = nums.length-1;
+        for(int i =0;i<= Math.min(max,n);i++)
+            max = Math.max(max,i+nums[i]);
+
+        return max>=n;
+    }
+
+}
