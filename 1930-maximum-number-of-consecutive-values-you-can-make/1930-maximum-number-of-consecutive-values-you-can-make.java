fdc7b17764ec68b5d5b3de6fa7131f02f572043c
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2025-10-27 01:10:38 +0300

    Time: 19 ms (4.85%), Space: 52.8 MB (90.29%) - LeetHub

--- 1930-maximum-number-of-consecutive-values-you-can-make/1930-maximum-number-of-consecutive-values-you-can-make.java
+++ 1930-maximum-number-of-consecutive-values-you-can-make/1930-maximum-number-of-consecutive-values-you-can-make.java
@@ -1,15 +1,13 @@
 class Solution {
-    public int getMaximumConsecutive(int[] coins) {
-
-        int[] arr = Arrays.copyOf(coins, coins.length+1);
-        arr[coins.length]= 0;
+    public int getMaximumConsecutive(int[] arr) {
         Arrays.sort(arr);
-        int i=0;
+        int i=-1;
+        int min=0;
         int curr = 0;
         int ans=-1;
-        for(int j=1;j<arr.length;j++)
+        for(int j=0;j<arr.length;j++)
         {
-            int min = arr[i];
+            min = i==-1?0:arr[i];
             int lower = arr[j];
             int upper = arr[j]+curr;
             if(curr+1>=lower && curr+1<=upper )
@@ -23,7 +21,7 @@ class Solution {
                 curr = min;
             }
         }
-        ans= Math.max(ans, curr - arr[i]+1);
+        ans= Math.max(ans, curr - min +1);
         return ans;
     }
 }
