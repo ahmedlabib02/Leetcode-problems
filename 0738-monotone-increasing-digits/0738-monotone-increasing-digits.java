2e875cf7651cc414eca5ab965d472e3360ab1190
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2025-10-28 02:12:49 +0300

    Time: 3 ms (7.25%), Space: 41.1 MB (13.33%) - LeetHub

---
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int monotoneIncreasingDigits(int n) {
+        String s = String.valueOf(n);
+        boolean isTight = true;
+        int[] minSuffix = new int[s.length()];
+        minSuffix[s.length()-1] = s.charAt(s.length()-1)-'0';
+        for(int i=s.length()-2;i>=0;i--)
+        {
+            char c= s.charAt(i);
+            int digit = c-'0';
+            if(digit > minSuffix[i+1])
+            {
+                minSuffix[i] = digit-1;
+            }
+            else
+            {
+                minSuffix[i] = digit;
+            }
+        }
+        System.out.println(Arrays.toString(minSuffix));
+        int num =0;
+        for(int i=0;i<s.length();i++)
+        {
+            char c= s.charAt(i);
+            int digit = c-'0';
+            if(isTight && digit>minSuffix[i])
+            {
+                num*=10;
+                num+= (minSuffix[i]);
+                isTight =false;
+            }
+            else if(isTight)
+            {
+                num*=10;
+                num+=digit;
+            }
+            else
+            {
+                num*=10;
+                num+=9;
+            }
+        }
+        return num;
+    }
+}
