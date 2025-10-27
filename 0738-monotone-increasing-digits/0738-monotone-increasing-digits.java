78a732c1ff83b0556f4913e47e6243eb9eff8c13
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2025-10-28 02:20:07 +0300

    Time: 1 ms (90.14%), Space: 40.9 MB (27.54%) - LeetHub

--- 0738-monotone-increasing-digits/0738-monotone-increasing-digits.java
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.java
@@ -1,45 +1,11 @@
 class Solution {
-    public int monotoneIncreasingDigits(int n) {
-        String s = String.valueOf(n);
-        boolean isTight = true;
-        int[] minSuffix = new int[s.length()];
-        minSuffix[s.length()-1] = s.charAt(s.length()-1)-'0';
-        for(int i=s.length()-2;i>=0;i--)
-        {
-            char c= s.charAt(i);
-            int digit = c-'0';
-            if(digit > minSuffix[i+1])
-            {
-                minSuffix[i] = digit-1;
-            }
-            else
-            {
-                minSuffix[i] = digit;
-            }
-        }
-        // System.out.println(Arrays.toString(minSuffix));
-        int num =0;
-        for(int i=0;i<s.length();i++)
-        {
-            char c= s.charAt(i);
-            int digit = c-'0';
-            if(isTight && digit>minSuffix[i])
-            {
-                num*=10;
-                num+= (minSuffix[i]);
-                isTight =false;
-            }
-            else if(isTight)
-            {
-                num*=10;
-                num+=digit;
-            }
-            else
-            {
-                num*=10;
-                num+=9;
-            }
-        }
-        return num;
+    public int monotoneIncreasingDigits(int N) {
+        char[] S = String.valueOf(N).toCharArray();
+        int i = 1;
+        while (i < S.length && S[i-1] <= S[i]) i++;
+        while (0 < i && i < S.length && S[i-1] > S[i]) S[--i]--;
+        for (int j = i+1; j < S.length; ++j) S[j] = '9';
+
+        return Integer.parseInt(String.valueOf(S));
     }
 }
