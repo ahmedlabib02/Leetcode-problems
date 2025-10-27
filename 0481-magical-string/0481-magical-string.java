54b7e024c61b085cc01ac0fb177edfb9d518baa8
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2025-10-28 02:52:44 +0300

    Time: 53 ms (8.32%), Space: 46.4 MB (5.92%) - LeetHub

---
+++ 0481-magical-string/0481-magical-string.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int magicalString(int n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(2);
+        int ans =1;
+        int pointer =2;
+        while(arr.size()<n)
+        {
+            if(arr.get(pointer)==1 && arr.getLast()==1 )
+            {
+                arr.add(2);
+                pointer++;
+            }
+            else if(arr.get(pointer)==1 && arr.getLast()==2)
+            {
+                ans++;
+                pointer++;
+                arr.add(1);
+            }
+            else if(arr.get(pointer)==2 && arr.getLast()==1)
+            {
+                arr.add(2);
+                arr.add(2);
+                pointer++;
+
+            }
+            else
+            {
+
+                arr.add(1);
+                ans++;
+                if(arr.size()>=n)
+                    break;
+                arr.add(1);
+                ans++;
+                pointer++;
+            }
+        }
+       System.out.println(arr);
+        return ans;
+    }
+}
