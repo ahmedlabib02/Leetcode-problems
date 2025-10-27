e1eb3d3de4b5e59ed21b7a49ad5e71e0b186f430
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2025-10-27 14:10:21 +0300

    Time: 25 ms (37.23%), Space: 55 MB (100%) - LeetHub

--- 1684-find-latest-group-of-size-m/1684-find-latest-group-of-size-m.java
+++ 1684-find-latest-group-of-size-m/1684-find-latest-group-of-size-m.java
@@ -2,48 +2,48 @@ class Solution {
    public int findLatestStep(int[] arr, int m) {
         if(m==arr.length)
             return m;
-        HashMap<Integer, int[]> map = new HashMap<>(arr.length);
+        int[][] intervals = new int[arr.length+2][2];
+        Arrays.fill(intervals, null);
         int ans =-1;
         for(int i=0;i<arr.length;i++)
         {
             int num = arr[i];
             int[] interval = null;
-            if(map.containsKey(num-1) && map.containsKey(num+1))
+            if(intervals[num-1]!=null && intervals[num+1]!=null)
             {
-                int[] leftInterval = map.get(num-1); // the num -1 is the upper bound in this interval
-                int[] rightInterval = map.get(num+1); // the num+1 is the lower bound in this interval
+                int[] leftInterval = intervals[num-1]; // the num -1 is the upper bound in this interval
+                int[] rightInterval = intervals[num+1]; // the num+1 is the lower bound in this interval
                 if(leftInterval[1]-leftInterval[0]+1==m || rightInterval[1]-rightInterval[0]+1==m)
                 {
                     ans = i; // record the latest step
                 }
                 leftInterval[1] = rightInterval[1];
-                map.put(leftInterval[0], leftInterval);
-                map.put(leftInterval[1], leftInterval);
+                intervals[leftInterval[1]] = leftInterval;
+
             }
-            else if(map.containsKey(num-1))
+            else if(intervals[num-1]!=null)
             {
-                interval =map.get(num-1); // the num -1 is the upper bound in this interval
+                interval =intervals[num-1]; // the num -1 is the upper bound in this interval
                 if(interval[1]-interval[0]+1==m)
                 {
                     ans = i; // record the latest step
                 }
-
                 interval[1]++;
-                map.put(num,interval);
+                intervals[num] = interval;
             }
-            else if(map.containsKey(num+1))
+            else if(intervals[num+1]!=null)
             {
-                interval =map.get(num+1); // the num+1 is the lower bound in this interval
+                interval =intervals[num+1]; // the num+1 is the lower bound in this interval
                 if(interval[1]-interval[0]+1==m)
                 {
                     ans = i; // record the latest step
                 }
                 interval[0]--;
-                map.put(num,interval);
+                intervals[num] = interval;
             }
             else
             {
-                map.put(num, new int[]{num, num});
+                intervals[num] = new int[]{num, num};
             }
         }
         return ans;
