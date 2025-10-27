2d6f10c5106023a50ca3fa6aab9282e0d6df0705
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2025-10-27 14:14:24 +0300

    Time: 6 ms (85.11%), Space: 58.5 MB (41.49%) - LeetHub

--- 1684-find-latest-group-of-size-m/1684-find-latest-group-of-size-m.java
+++ 1684-find-latest-group-of-size-m/1684-find-latest-group-of-size-m.java
@@ -1,51 +1,16 @@
 class Solution {
-   public int findLatestStep(int[] arr, int m) {
-        if(m==arr.length)
-            return m;
-        int[][] intervals = new int[arr.length+2][2];
-        Arrays.fill(intervals, null);
-        int ans =-1;
-        for(int i=0;i<arr.length;i++)
-        {
-            int num = arr[i];
-            int[] interval = null;
-            if(intervals[num-1]!=null && intervals[num+1]!=null)
-            {
-                int[] leftInterval = intervals[num-1]; // the num -1 is the upper bound in this interval
-                int[] rightInterval = intervals[num+1]; // the num+1 is the lower bound in this interval
-                if(leftInterval[1]-leftInterval[0]+1==m || rightInterval[1]-rightInterval[0]+1==m)
-                {
-                    ans = i; // record the latest step
-                }
-                leftInterval[1] = rightInterval[1];
-                intervals[leftInterval[1]] = leftInterval;
-
-            }
-            else if(intervals[num-1]!=null)
-            {
-                interval =intervals[num-1]; // the num -1 is the upper bound in this interval
-                if(interval[1]-interval[0]+1==m)
-                {
-                    ans = i; // record the latest step
-                }
-                interval[1]++;
-                intervals[num] = interval;
-            }
-            else if(intervals[num+1]!=null)
-            {
-                interval =intervals[num+1]; // the num+1 is the lower bound in this interval
-                if(interval[1]-interval[0]+1==m)
-                {
-                    ans = i; // record the latest step
-                }
-                interval[0]--;
-                intervals[num] = interval;
-            }
-            else
-            {
-                intervals[num] = new int[]{num, num};
-            }
+   public int findLatestStep(int[] A, int m) {
+        int res = -1, n = A.length;
+        int[] length = new int[n + 2], count = new int[n + 1];
+        for (int i = 0; i < n; ++i) {
+            int a = A[i], left = length[a - 1], right = length[a + 1];
+            length[a] = length[a - left] = length[a + right] = left + right + 1;
+            count[left]--;
+            count[right]--;
+            count[length[a]]++;
+            if (count[m] > 0)
+                res = i + 1;
         }
-        return ans;
+        return res;
     }
 }
