1f80a3b6df43f929eb2467141da251d8ed4042e8
ahmedlabib02 <43939888+ahmedlabib02@users.noreply.github.com>
2025-10-30 00:12:52 +0300

    Time: 52 ms (19.85%), Space: 46.7 MB (90.38%) - LeetHub

---
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        HashMap<Integer, Integer> cnt = new HashMap<>();
+        int unique=0;
+        int i =0;
+        int x=0;
+        int ans =0;
+        for(int j=0;j<nums.length;j++)
+        {
+            cnt.put(nums[j], cnt.getOrDefault(nums[j],0)+1);
+            if(cnt.get(nums[j])==1)
+                unique++;
+            if(unique==k)
+            {
+                while(x<j && unique==k)
+                {
+                    cnt.put(nums[x], cnt.get(nums[x])-1);
+                    if(cnt.get(nums[x])==0)
+                        unique--;
+                    x++;
+                }
+                if(unique<k)
+                {
+                    x--;
+                cnt.put(nums[x], cnt.get(nums[x])+1);
+                if(cnt.get(nums[x])==1)
+                    unique++;
+                }
+                
+                ans+=x-i+1;
+            }
+            else if(unique>k) {
+                i=x;
+                while (unique > k) {
+                    cnt.put(nums[i], cnt.get(nums[i]) - 1);
+                    if (cnt.get(nums[i]) == 0)
+                        unique--;
+                    i++;
+                }
+                x=i;
+                while (x < j && unique == k) {
+                    cnt.put(nums[x], cnt.get(nums[x]) - 1);
+                    if (cnt.get(nums[x]) == 0)
+                        unique--;
+                    x++;
+                }
+            if(unique<k)
+            {
+                x--;
+                cnt.put(nums[x], cnt.get(nums[x]) + 1);
+                if(cnt.get(nums[x])==1)
+                    unique++;
+            }
+                
+                
+                
+                ans += x - i + 1;
+            }
+        }
+        return ans;
+    }
+}
